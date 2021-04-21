59f1ac550ef66821b83d032d4ef95c311b903108
Lee X <lee52597@gmail.com>
2021-04-21 10:18:48 -0400

    Final commit

---
+++ DjangoRestAPI/music/views.py
@@ -0,0 +1,45 @@
+from .models import Song
+from .serializers import SongSeralizer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+
+# Create your views here.
+class SongList(APIView):
+
+    def get(self, request):
+        song = Song.objects.all()
+        serializer = SongSeralizer(song, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = SongSeralizer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class SongDetail(APIView):
+
+    def get(self, request, pk):
+        song = self.get_object(pk)
+        serializer = SongSeralizer(song)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk):
+        song = self.get_object(pk)
+        serializer = SongSeralizer(song)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def get_object(self, pk):
+        try:
+            return Song.objects.get(pk=pk)
+        except Song.DoesNotExist:
+            raise Response(status=status.HTTP_404_NOT_FOUND)
+
+    def delete(self, request, pk):
+        song = self.get_object(pk)
+        song.delete()
+        return Response(status=status.HTTP_200_OK)
